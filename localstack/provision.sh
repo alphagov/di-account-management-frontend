250136e300761bba645016e51ee186d981af6d76
divyaparameswaran <divya.parameswaran@digital.cabinet-office.gov.uk>
2022-12-13 11:18:12 +0000

    Update localstack provision script with the correct fields for the table

--- localstack/provision.sh
+++ localstack/provision.sh
@@ -8,7 +8,7 @@
 export TABLE_NAME=user_services
 export TABLE_SCHEMA_SEED=user_services_table_seed.json
 export AWS_PROFILE=test-profile
-export BUILD_CLIENT_ID=nXx0aoWYfQr13mhQ1Kr9WLAOR7WMQZFmRT4-Yoq7Uw0
+export BUILD_CLIENT_ID=nSrKLhBOWx9qRFeiwFY-bz8toTHrjBaXVZi3nP7rS6c
 
 aws --endpoint-url=http://localhost:4566 dynamodb create-table \
     --cli-input-json "$(cat  ./localstack/$TABLE_SCHEMA_SEED)" \
@@ -26,7 +26,7 @@ aws --endpoint-url=http://localhost:4566 dynamodb put-item \
     --region eu-west-2 \
     --profile $AWS_PROFILE \
     --item \
-        "{\"user_id\":{\"S\": \"$BUILD_CLIENT_ID\"}, \"services\": {\"L\":[{\"M\":{\"count_successful_logins\":{\"N\":\"2\"},\"client_id\":{\"S\":\"client_id_1\"},\"last_accessed\":{\"N\":\"1666169856\"}}}]}}"
+        "{\"user_id\":{\"S\": \"$BUILD_CLIENT_ID\"}, \"services\": {\"L\":[{\"M\":{\"count_successful_logins\":{\"N\":\"2\"},\"client_id\":{\"S\":\"gov-uk\"},\"last_accessed\":{\"N\":\"1666169856\"},\"last_accessed_pretty\":{\"S\":\"20 January 1970\"}}}]}}"
 
 aws --endpoint-url=http://localhost:4566 dynamodb get-item \
     --table-name $TABLE_NAME  \
